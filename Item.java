2fd795e25e462b793d273e48805520982afced1e
Tamara Tanevska <tamara.marko@live.com>
2020-04-12 17:07:42 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -6,12 +6,46 @@ class Item {
      enum danok d;
 
 	//TODO add variable.
+     Item(int id,String name,double price,danok d)
+     {
+      id=id;
+      this.name=name;
+      price=price;
+      this.d=d;
+      }
+
 
 	//TODO constructor
 
 	//TODO setters and getters
+     
+      public String getName()
+      {
+        return name;
+      }
+      public String setName(String name)
+      {
+       this.name=name;
+      } 
+      public int getId()
+      {
+        return id;
+      }
+      public id setId(int id)
+      {
+       id=id;
+      } 
+      public doule getPrice()
+      {
+        return price;
+      }
+      public double setPrice(double price)
+      {
+       price=price;
+      } 
 
-	double taxReturn () {
-		//TODO
+
+	double taxReturn (double price) {
+		return 15*price/100;
 	}
 }
